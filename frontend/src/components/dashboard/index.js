e56484085c2689e31a03ba60a40a3e14993065e8
MintShards <mintryukishi@gmail.com>
2025-11-09 01:43:42 -0800

    removed unused files

--- frontend/src/components/dashboard/index.js
+++ frontend/src/components/dashboard/index.js
@@ -1,4 +1,2 @@
 // Dashboard component exports for easy imports
 export { default as QuickActionsSection } from './QuickActionsSection';
-export { default as SystemStatus } from './SystemStatus';
-export { default as RecentActivity } from './RecentActivity';
